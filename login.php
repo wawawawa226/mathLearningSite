5c13cee0170958a6a8bb30cfe0e84ae04116c5b2
DAIGO <wawawawa226@users.noreply.github.com>
2021-01-21 21:08:30 +0900

    login.phpを追加

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+$_SESSION['login_error'] = "";
+
+function h($var) //HTMLでのエスケープ処理をする関数
+{
+  if(is_array($var)){
+    return array_map('h',$var);
+  }else{
+    return htmlspecialchars($var,ENT_QUOTES,'UTF-8');
+  }
+}
+
+
+
+// if( $mysqli->connect_errno ) {
+// echo $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
+// }
+// $mysqli->set_charset('utf8');
+//
+// $email = $_POST['email'] ;
+//
+// $sql = "SELECT * FROM user_info WHERE mail='$email'";
+//
+// $res = $mysqli->query($sql);
+//
+//
+// $data = $res->fetch_assoc();
+
+
+//emailがDB内に存在しているか確認
+if (!isset($data['mail'])) {
+  $_SESSION['login_error'] = "エラー";
+  header("Location: http://wawawawa.pigboat.jp/samurai/login-form.php?email=$email");
+  return false;
+}
+
+$pass = $_POST['pass'];
+
+if (password_verify("$pass", $data['password'])) {
+  // ログイン成功
+  session_regenerate_id(true); //session_idを新しく生成し、置き換える
+  $_SESSION['NAME'] = $data['user_name'];
+  header('Location: http://wawawawa.pigboat.jp/samurai/mypage.php');
+  exit;
+} else {
+  $_SESSION['login_error'] = "エラー";
+  header("Location: http://wawawawa.pigboat.jp/samurai/login-form.php?email=$email");
+  return false;
+}
+
+
+?>
