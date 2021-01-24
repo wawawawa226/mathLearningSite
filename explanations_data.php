323e7beb7655bf1416f659613a495bebd4991ebe
DAIGO <wawawawa226@users.noreply.github.com>
2021-01-24 22:35:37 +0900

    seihuをseifuに変更

--- explanations_data.php
+++ explanations_data.php
@@ -1,12 +1,12 @@
 <?php
 require_once ('Explanations_class.php');
-$seihu = new Exp('正負の数','images/seihu.jpeg','images/seihu.jpeg');
-$moji = new Exp('文字式','images/seihu.jpeg','images/seihu.jpeg');
-$hoteishiki = new Exp('方程式','images/seihu.jpeg','images/seihu.jpeg');
-$kansu = new Exp('関数','images/seihu.jpeg','images/seihu.jpeg');
-$heimen = new Exp('平面図系','images/seihu.jpeg','images/seihu.jpeg');
-$kukan = new Exp('空間図形','images/seihu.jpeg','images/seihu.jpeg');
-$shiryo = new Exp('資料の整理','images/seihu.jpeg','images/seihu.jpeg');
+$seifu = new Exp('正負の数','images/seifu.jpeg','images/seifu.jpeg');
+$moji = new Exp('文字式','images/seifu.jpeg','images/seifu.jpeg');
+$hoteishiki = new Exp('方程式','images/seifu.jpeg','images/seifu.jpeg');
+$kansu = new Exp('関数','images/seifu.jpeg','images/seifu.jpeg');
+$heimen = new Exp('平面図系','images/seifu.jpeg','images/seifu.jpeg');
+$kukan = new Exp('空間図形','images/seifu.jpeg','images/seifu.jpeg');
+$shiryo = new Exp('資料の整理','images/seifu.jpeg','images/seifu.jpeg');
 
-$mathUnits = array($seihu,$moji,$hoteishiki,$kansu,$heimen,$kukan,$shiryo);
+$mathUnits = array($seifu,$moji,$hoteishiki,$kansu,$heimen,$kukan,$shiryo);
 ?>
