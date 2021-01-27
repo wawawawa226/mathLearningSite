3bb6bdd58667a5b8ed7fd9a7d2e673a3deb0271b
Daigo <dgotks350@gmail.com>
2021-01-27 21:52:22 +0900

    memoを保存するためのSQL文を作成

---
+++ memoAjax.php
@@ -0,0 +1,19 @@
+<?php
+  require_once ('Common.php');
+  session_start();
+  if(isset($_SESSION['id'])){
+    $id = $_SESSION['id'] ;
+  }
+  $memo = $_POST['memo'];
+  $dbh = db_connect();
+  $res = $dbh->prepare('INSERT INTO memo(user_id,memo,date) VALUES(:id,:memo,now())');
+  $res->bindValue(':id',(int)$id,PDO::PARAM_INT);
+  $res->bindValue(':memo',(string)$memo,PDO::PARAM_STR);
+  $res->execute();
+  $data = 1;
+
+  header("Content-type: application/json; charset=UTF-8");
+  //JSONデータを出力
+  echo json_encode($data);
+  exit;
+?>
