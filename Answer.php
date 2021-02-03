d81ef6b72ec497d3a4df5f3bc4e034537cd4d5fc
Daigo <dgotks350@gmail.com>
2021-02-03 17:34:26 +0900

    ファイル名をアッパーキャメルケースに統一

---
+++ Answer.php
@@ -0,0 +1,112 @@
+<?php
+  require_once 'Common.php';
+  session_start();
+  if(isset($_SESSION['name'])){
+    // ログインしている場合
+    $userName = $_SESSION['name'];
+  }else{
+    // ログインしていない場合、名前をゲストにする
+      $userName = "ゲスト";
+      $loginCheck = "none";
+    }
+
+  $key = array_rand($works, 1);
+?>
+
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- BootstrapのCSS読み込み -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/answer.css">
+    <title>ランダムテスト</title>
+  </head>
+  <body>
+    <header>
+      <?php include __DIR__ . '/Header.php';?>
+    </header>
+    <div class="mypage-contents wrapper">
+    <main>
+  <h2>総合ランダムテスト</h2>
+  <div class="item">
+    <p>難易度と単元を選択して「問題を表示する」を押すと、問題がランダムで出題されます。</p>
+    <p>一度問題を表示した後は「問題を変更する」を押すと別の問題に変更することができます。</p>
+  </div>
+
+      <form action="Answer.php" method="post" id="sign_up" name="su">
+      <hr>
+      <label class="control-label">難易度を選択してください</label>
+      <div class="form-group buttons">
+      <?php
+      foreach ($work_levels as $key => $val) {
+        $check = "";
+        if($key === 1){
+          $check = 'checked' ;
+        }
+      echo '<div class="form-check-inline">';
+      echo "<input class=\"form-check-input\" type=\"radio\" name=\"level\" id=\"level".$key."\" value=\"".$key."\"".$check.">";
+      echo "<label class=\"form-check-label\" for=\"level".$key."\">".$val."</label>";
+      echo '</div>';
+    }
+      echo '</div>';
+    ?>
+    <hr>
+    <div class="form-group">
+      <label class="control-label">単元を選択してください</label>
+        <select class="form-control short-form" name="unit" id="unit">
+          <?php
+          foreach($units as $val){
+             $check = "";
+             if($val == $unit){
+               $check = 'selected';
+             }
+            echo "<option value=\"" .$val. "\"" .$check.">".$val."</option>";
+          }
+          ?>
+        </select>
+      </div>
+
+      <hr>
+
+      <div class="form-droup">
+    <button type="button" id="workDisplay" name="btn_confirm" class="" style="margin:2em 0em;">問題を表示する</button>
+      </div>
+      <hr>
+    <div id="question">
+      <div class="form-group">
+        <label class="control-label" for="work">問題</label>
+        <textarea class="form-control" name="work" id="work" readonly></textarea>
+      </div>
+
+      <input type="hidden" id= "work_number" name="work_number" value="">
+      <hr>
+      <div class="form-group">
+        <label class="control-label" for="work_answer">解答</label>
+        <textarea class="form-control" name="work_answer" id="work_answer" placeholder="解答を入力"></textarea>
+      </div>
+    <div class="form-droup">
+  <button type="button" id="answer" name="btn_confirm" class="btn btn-outline-info btn-lg" style="margin:2em 0em;">回答する</button>
+    </div>
+  </div>
+
+</form>
+</main>
+
+<aside>
+  <?php include __DIR__ . '/Sidebar.php';?>
+</aside>
+
+  </div>
+
+  <footer>
+    <?php include __DIR__ . '/Footer.php';?>
+  </footer>
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/workbook.js"></script>
+  <script src="js/answer.js"></script>
+</body>
+</html>
---
+++ Login.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+require_once ('Common.php');
+if(isset($_SESSION['id'])){
+  $_SESSION['message'] = "既にログインしています。";
+  header("Location:" . $url_mypage );
+  exit();
+}
+$_SESSION['login_error'] = "";
+
+function h($var) //HTMLでのエスケープ処理をする関数
+{
+  if(is_array($var)){
+    return array_map('h',$var);
+  }else{
+    return htmlspecialchars($var,ENT_QUOTES,'UTF-8');
+  }
+}
+if(isset($_POST['email'])){
+  $email = $_POST['email'] ;
+  $dbh = db_connect();
+  $prepare = $dbh->prepare("SELECT * FROM user_info WHERE mail=:email");
+  $prepare->bindValue(':email',(string)$email,PDO::PARAM_STR);
+  $prepare->execute();
+  $data = $prepare->fetch(PDO::FETCH_ASSOC);
+}
+
+//emailがDB内に存在しているか確認
+if (!isset($data['mail'])) {
+  $_SESSION['login_error'] = "エラー";
+  header("Location:" . $url_loginForm );
+  exit();
+}
+
+$pass = $_POST['pass'];
+
+if (password_verify("$pass", $data['password'])) {
+  // ログイン成功
+  session_regenerate_id(true); //session_idを新しく生成し、置き換える
+  $_SESSION = array();
+  $_SESSION['name'] = $data['user_name'];
+  $_SESSION['id'] = $data['user_id'];
+  $_SESSION['age'] = $data['age'];
+  $_SESSION['math_lev'] = $data['math_lev'];
+  $_SESSION['math_like'] = $data['math_like'];
+  $_SESSION['club'] = $data['club'];
+  header("Location:" . $url_mypage );
+  exit();
+} else {
+  $_SESSION['login_error'] = "エラー";
+  header("Location:" . $url_loginForm );
+  exit();
+}
+
+
+?>
